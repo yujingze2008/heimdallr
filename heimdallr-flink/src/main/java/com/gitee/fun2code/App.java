5d897ca10d2b3005b6d10b23a134f0598ad441cc
513252223@qq.com <jz61452789>
2019-08-13 01:22:47 +0800

    flink test

--- heimdallr-flink/src/main/java/com/gitee/fun2code/App.java
+++ heimdallr-flink/src/main/java/com/gitee/fun2code/App.java
@@ -1,7 +1,9 @@
 package com.gitee.fun2code;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
 import org.apache.flink.types.Row;
@@ -16,10 +18,25 @@ public class App
     {
         final ExecutionEnvironment environment = ExecutionEnvironment.createLocalEnvironment();
         DataSet<String> ds = environment.readTextFile("hdfs://cdh01.com:8020//user/hive/warehouse/datax_hive");
-
+        /*DataSet<Tuple3<Integer,String,Integer>> newDs = ds.map(new MapFunction<String, Tuple3<Integer,String,Integer>>() {
+            @Override
+            public Tuple3<Integer,String,Integer> map(String s) throws Exception {
+                String[] ss = s.split(",");
+                Tuple3 tuple3 = new Tuple3(Integer.parseInt(ss[0]),ss[1],Integer.parseInt(ss[2]));
+                return tuple3;
+            }
+        });*/
+        DataSet<NewEntity> newDs = ds.map(new MapFunction<String, NewEntity>() {
+            @Override
+            public NewEntity map(String s) throws Exception {
+                String[] ss = s.split(",");
+                return new NewEntity(Integer.parseInt(ss[0]),ss[1],Integer.parseInt(ss[2]));
+            }
+        });
         BatchTableEnvironment bte = BatchTableEnvironment.create(environment);
-        Table table = bte.fromDataSet(ds,"id,name,age");
-        Table counts = bte.sqlQuery("select id from " + table);
+        Table table = bte.fromDataSet(newDs,"id,name,age");
+        Table counts = bte.sqlQuery("select count(age) from " + table);
         bte.toDataSet(counts,Row.class).print();
     }
+
 }
